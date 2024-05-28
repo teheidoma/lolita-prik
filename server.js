6809ea25502bb153d5ac41904d3b9cdeca728b4d
teheidoma <novakovskii2002@gmail.com>
2024-05-28 03:36:32 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.use(express.static("public"))
+
+app.use('/', function (req, res) {
+    res.sendFile(path.resolve(__dirname, "public/index.html"))
+})
+
+app.listen(port, () => {
+    console.log("Server running on port " + port)
+})
